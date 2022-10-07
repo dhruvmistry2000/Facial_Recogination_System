c6108b0adee7e2f0bdb01629063955a14ca1c772
Dhruv <dhruvmistry2000@gmail.com>
2022-10-07 21:55:31 +0530

    Adding Files(Dhruv)

---
+++ Fliter2.py
@@ -0,0 +1,12 @@
+a=0
+b=0
+c=0
+d=0
+
+
+
+for a in range(0,255):
+    for b in range(0,255):
+        for c in range(0,255):
+            for d in range(0,255):
+                print(a,b,c,d)
--- Imge_Filter.py
+++ Imge_Filter.py
@@ -1,42 +1,29 @@
 import cv2
 import numpy as np
 
-path = 'img1.jpg'
+path = 'img44.jpg'
 
 def empty(a):
     pass
 
-cv2.namedWindow("trackbar")
-cv2.resizeWindow("trackbar", 640, 240)
-cv2.createTrackbar("Hue_Min", "trackbar", 0, 179, empty)
-cv2.createTrackbar("Hue_Max", "trackbar", 176, 179, empty)
-cv2.createTrackbar("Sat_Min", "trackbar", 0, 255, empty)
-cv2.createTrackbar("Sat_Max", "trackbar", 255, 255, empty)
-cv2.createTrackbar("Val_Min", "trackbar", 0, 255, empty)
-cv2.createTrackbar("Val_Max", "trackbar", 255, 255, empty)
-
-
-while True:
-    img = cv2.imread(path)
-    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    h_min = cv2.getTrackbarPos("Hue_Min", "trackbar")
-    h_max = cv2.getTrackbarPos("Hue_Max", "trackbar")
-    s_min = cv2.getTrackbarPos("Sat_Min", "trackbar")
-    s_max = cv2.getTrackbarPos("Sat_Max", "trackbar")
-    v_min = cv2.getTrackbarPos("Val_Min", "trackbar")
-    v_max = cv2.getTrackbarPos("Val_Max", "trackbar")
-
-    lower = np.array([h_min,s_min,v_min])
-    upper = np.array([h_max,s_max,v_max])
-    mask = cv2.inRange(img2, lower, upper)
-    imgresult = cv2.bitwise_and(img, img , mask=mask)
-
-    cv2.imshow("img", img)
-    cv2.imshow("HSV", img2)
-    cv2.imshow("mask", mask)
-    cv2.imshow("result", imgresult)
-
-    cv2.waitKey(1)
+hue = 0
+sat = 0
+val = 0
+for hue in range(0, 179):
+    for sat in range(0, 255):
+        for val in range(0, 255):
+            img = cv2.imread(path)
+            imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            lower = np.array([hue, sat, val])
+            upper = np.array([179, 255, 255])
+            mask = cv2.inRange(imgHSV, lower, upper)
+            imgResult = cv2.bitwise_and(img, img, mask=mask)
+            cv2.imshow("Original", img)
+            cv2.imshow("HSV", imgHSV)
+            cv2.imshow("Mask", mask)
+            cv2.imshow("Result", imgResult)
+            cv2.waitKey(1)
+            print(hue, sat, val)
 
 
 
