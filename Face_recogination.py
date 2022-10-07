564586f45c2d12376e7ae442c5639a56f333d5fc
Dhruv <dhruvmistry2000@gmail.com>
2022-10-07 10:48:02 +0530

    Adding Files(Dhruv)

---
+++ Face_recogination.py
@@ -0,0 +1,25 @@
+# face recognition
+import cv2
+import numpy as np
+import sys
+import time
+
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+cap.set(10, 100)
+while True:
+    success, img = cap.read()
+    imggray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(imggray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 0), 2)
+    cv2.imshow("Video", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cv2.imshow("Video", imggray)
+cap.release()
+cv2.destroyAllWindows()
+
