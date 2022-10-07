0c01c9e2336b49b4872785d2201ac83c5d5fa49d
Dhruv <dhruvmistry2000@gmail.com>
2022-10-07 22:57:57 +0530

    Adding Files(Dhruv)

--- Notification.py
+++ Notification.py
@@ -9,16 +9,3 @@ while True:
         app_icon = "Notif.ico",
         timeout = 10,
     )
-
-# import byte
-# import socket
-#
-# host = '10.0.4.194'
-# port = 34000
-#
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect((host, port))
-# for i in range(10):
-#     message = bytes('some message', 'utf-8')
-#     s.send(message)
-# s.close()
