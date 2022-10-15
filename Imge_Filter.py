cd3e6677bdcb4e10aaf9f9846e067c90924ca560
Dhruv <dhruvmistry2000@gmail.com>
2022-10-15 14:09:00 +0530

    Adding Files(Dhruv)

--- Imge_Filter.py
+++ Imge_Filter.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
 import os
+import time
 
+
+start_time = time.time()
 
 path = 'Photos/img44.jpg'
 path2 = 'D:\CV_Photos'
@@ -13,9 +16,9 @@ hue = 0
 sat = 0
 val = 0
 i=0
-for hue in range(0, 179):
-    for sat in range(0, 255):
-        for val in range(0, 255):
+for hue in range(0, 179,5):
+    for sat in range(0, 255,5):
+        for val in range(0, 255,5):
             img = cv2.imread(path)
             imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
             lower = np.array([hue, sat, val])
@@ -27,6 +30,9 @@ for hue in range(0, 179):
             cv2.imwrite(os.path.join(path2, 'black-white_'+str(i)+'.jpg'), imgResult)
             i=i+1
             cv2.waitKey(0)
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
 
 
 
